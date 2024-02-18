efcba998f94e86f8595634f0bde4abd0e814014a
GreysonPerrin <greyson.perrin@gmail.com>
2024-02-18 16:36:25 -0500

    Added comments to car scripts
    Added comments to the Car_Hazard and Car_Generator scripts to explain how they work

--- root/JumpyStreetGame/Assets/Scripts/Hazards/Car_Generator.cs
+++ root/JumpyStreetGame/Assets/Scripts/Hazards/Car_Generator.cs
@@ -4,34 +4,35 @@ using UnityEngine;
 
 public class Car_Generator : MonoBehaviour
 {
-    [SerializeField] private GameObject[] carPrefabs;
-    [SerializeField] private float minSpawnTime;
-    [SerializeField] private float maxSpawnTime;
-    private float timer = 0;
-    private float spawnTime = 0;
+    [SerializeField] private GameObject[] carPrefabs; // all of the cars that be spawned
+    [SerializeField] private float minSpawnTime; // minimum time in seconds before a new car spawns
+    [SerializeField] private float maxSpawnTime; // maximum time in seconds before a new car spawns
+    private float timer = 0; // tracks how much time has passed since spawning a car
+    private float spawnTime = 0; // amount of time before spawning another car; randomly generated each time
 
+    // These two variables are accessed by the Car_Hazard scripts
     public Transform startPoint;
     public Transform endPoint;
 
     void Start()
     {
-        SpawnCar();
+        SpawnCar(); // Immediately spawn a car
     }
 
     void Update()
     {
-        timer += Time.deltaTime;
+        timer += Time.deltaTime; // Timer increases over time
         if (timer > spawnTime)
         {
-            SpawnCar();
+            SpawnCar(); // Once enough time has passed, spawn a new car
         }
     }
 
     private void SpawnCar()
     {
-        int i = Random.Range(0, carPrefabs.Length);
-        Instantiate(carPrefabs[i], gameObject.transform.position, gameObject.transform.rotation, gameObject.transform);
-        timer = 0;
-        spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+        int i = Random.Range(0, carPrefabs.Length); // Randomly decide which car to spawn
+        Instantiate(carPrefabs[i], gameObject.transform.position, gameObject.transform.rotation, gameObject.transform); // Spawn the car
+        timer = 0; // Reset the timer
+        spawnTime = Random.Range(minSpawnTime, maxSpawnTime); // Randomly decide how much time before the next car is spawned
     }
 }
--- root/JumpyStreetGame/Assets/Scripts/Hazards/Car_Hazard.cs
+++ root/JumpyStreetGame/Assets/Scripts/Hazards/Car_Hazard.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class Car_Hazard : MonoBehaviour
 {
-    [SerializeField] private float speed = 0.25f;
+    [SerializeField] private float speed = 0.25f; // how fast the cars move
 
-    [SerializeField] private float lerpRatio = 0;
+    [SerializeField] private float lerpRatio = 0; // controls how far along the car is: 0 - startpoint 1 - endpoint
 
-    private Transform startPoint;
-    private Transform endPoint;
+    private Transform startPoint; // point where the car spawns
+    private Transform endPoint; // point where the car despawns
 
     void Start()
     {
+        // The car is spawned as a child of the Car Spawner object
+        // The Car_Generator script contains variables for the start and end points
         startPoint = gameObject.GetComponentInParent<Car_Generator>().startPoint;
         endPoint = gameObject.GetComponentInParent<Car_Generator>().endPoint;
     }
 
     void Update()
     {
+        // the lerpRatio variable increases over time and is used the move the car to the endpoint
         lerpRatio += (Time.deltaTime * speed);
         gameObject.transform.position = Vector3.Lerp(startPoint.position, endPoint.position, lerpRatio);
+        // Once the car reaches the endpoint, it destroys itself
         if (lerpRatio >= 1f)
         {
             Destroy(gameObject);
